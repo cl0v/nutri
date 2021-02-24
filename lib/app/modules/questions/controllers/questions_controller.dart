3242f230b49fa74e3272cacd1a46d9ff036f8000
Marcelo Fernandes <marcelofv12@hotmail.com>
2021-02-23 23:55:26 -0300

    n sei o q é isso

--- lib/app/modules/questions/controllers/questions_controller.dart
+++ lib/app/modules/questions/controllers/questions_controller.dart
@@ -3,15 +3,24 @@ import 'package:get/get.dart';
 import 'package:nutri/app/data/model/question_model.dart';
 import 'package:nutri/app/data/repositories/question_repository.dart';
 import 'package:nutri/app/routes/app_pages.dart';
+<<<<<<< HEAD
 
 class QuestionsController extends GetxController {
   final QuestionRepository repository;
 
+=======
+import 'package:nutri/constants.dart';
+
+class QuestionsController extends GetxController {
+  final UserPreferencesRepository userDataRepository;
+  final QuestionRepository questionRepository;
+>>>>>>> parent of d2dbd33 (Alterações de melhorias na question page)
   QuestionsController({
     @required this.repository,
   });
 
   PageController _pageController = PageController();
+
   PageController get pageController => this._pageController;
 
   RxList<QuestionModel> _questions = <QuestionModel>[].obs;
@@ -20,9 +29,6 @@ class QuestionsController extends GetxController {
 
   var _isAnswered = false.obs;
   bool get isAnswered => _isAnswered.value;
-
-  RxInt _selectedIndex = 5.obs;
-  int get selectedIndex => _selectedIndex.value;
 
   final _index = RxInt();
   int get index => this._index.value;
@@ -37,10 +43,15 @@ class QuestionsController extends GetxController {
     _questions.assignAll(await repository.loadQuestionList());
   }
 
+<<<<<<< HEAD
   onQuestionPageChanged(int idx){
     lockedAnswer = false;
   }
 
+=======
+//TODO: BUGFIX: Se eu clickar rapido, a pagination pode pular mais de uma pagina(2x click)
+//TODO: O Questions deve receber a altura e peso do cliente
+>>>>>>> parent of d2dbd33 (Alterações de melhorias na question page)
   @override
   void onClose() {
     _pageController.dispose();
@@ -51,27 +62,13 @@ class QuestionsController extends GetxController {
   }
 
   Map<String, String> _answers = {};
-  var indexDaOpcao = -1;
 
-
-  int getOptionIndex(int qIdx, String opt) =>
-      questions[qIdx].options.indexOf(opt);
-
-  bool lockedAnswer = false;
-  onAnswerTapped(int qIdx, String opt) {
-    if (lockedAnswer) return;
-    lockedAnswer = true;
-    _selectedIndex.value = questions[qIdx].options.indexOf(opt);
-    //se o index for a mesma que a questao, marca saporra
-    // selectedIndex = idx;
-    // _answers[question.prefs] = question.options[index];
-    // _isSelectedQ.value = selectedIndex == questions[qIdx].options.indexOf(opt);
+  onAnswerTapped(QuestionModel question, int index) {
+    _answers[question.prefs] = question.options[index];
     _isAnswered.value = true;
     _index.value = index;
 
-//Receber o index da pagina, vai saber qual questao está sendo gerada
-//Receber o index da opçao marcada vai saber qual opçao deve ter qual cor
-    Future.delayed(Duration(milliseconds: 500), () {
+    Future.delayed(Duration(seconds: 1), () {
       _nextQuestion();
     });
   }
@@ -80,9 +77,15 @@ class QuestionsController extends GetxController {
     repository.setQuestionsPrefs(ans);
   }
 
+  Color getTheRightColor(idx) {
+    if (isAnswered) {
+      if (idx == this.index) return kGreenColor;
+    }
+    return kGrayColor;
+  }
+
   _nextQuestion() {
     if (pageController.page.round() != questions.length - 1) {
-      _selectedIndex.value = -1;
       _isAnswered.value = false;
       _pageController.nextPage(
           duration: Duration(milliseconds: 250), curve: Curves.ease);
